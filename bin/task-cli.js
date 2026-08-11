3fc903d68502ce44e8ffc51366461d0a97803e3b
vitorolivacoelho <vitorolivacoelho.p@proton.me>
2026-08-10 21:41:15 -0300

    feat: implement add command end to end

--- bin/task-cli.js
+++ bin/task-cli.js
@@ -1,6 +1,11 @@
+const { add } = require('../src/commands');
+
 const args = process.argv.slice(2);
 const command = args[0];
 const commandArgs = args.slice(1);
 
-console.log("Comando:", command);
-console.log("Argumento:", commandArgs);
+if (command === 'add') {
+  add(commandArgs[0]);
+} else {
+  console.log(`Comando desconhecido: ${command}`);
+}
