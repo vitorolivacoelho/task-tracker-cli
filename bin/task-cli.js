ed14e0da01e2bc529d5f7ae093a0e44620ef9a0a
vitorolivacoelho <vitorolivacoelho.p@proton.me>
2026-08-11 21:36:54 -0300

    feat: implement unfiltered list command

--- bin/task-cli.js
+++ bin/task-cli.js
@@ -1,4 +1,4 @@
-const { add } = require('../src/commands');
+const { add, list } = require('../src/commands');
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -6,6 +6,8 @@ const commandArgs = args.slice(1);
 
 if (command === 'add') {
   add(commandArgs[0]);
+} else if (command === 'list') {
+  list();
 } else {
   console.log(`Comando desconhecido: ${command}`);
 }
