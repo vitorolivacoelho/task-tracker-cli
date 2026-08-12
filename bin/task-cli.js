e3e1c44eedee9d93651904a0137fc0a5f071b054
vitorolivacoelho <vitorolivacoelho.p@proton.me>
2026-08-11 23:11:53 -0300

    feat: implement update, delete, markmark-in-progress & mark-completed command

--- bin/task-cli.js
+++ bin/task-cli.js
@@ -1,4 +1,4 @@
-const { add, list } = require('../src/commands');
+const { add, list, update, deleteTask, markInProgress, markCompleted } = require('../src/commands');
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -8,6 +8,14 @@ if (command === 'add') {
   add(commandArgs[0]);
 } else if (command === 'list') {
   list();
+} else if (command === 'update') {
+  update(commandArgs[0], commandArgs[1]);
+} else if (command === 'delete') {
+  deleteTask(commandArgs[0]);
+} else if (command === 'mark-in-progress') {
+  markInProgress(commandArgs[0]);
+} else if (command === 'mark-completed') {
+  markCompleted(commandArgs[0]);
 } else {
   console.log(`Comando desconhecido: ${command}`);
 }
